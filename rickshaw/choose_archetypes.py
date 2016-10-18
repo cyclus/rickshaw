18a47285000dcb23f166905c90f5ca630b955d1f
Anthony Scopatz <scopatz@gmail.com>
2016-10-18 12:41:40 -0400

    some more structure

--- rickshaw/choose_archetypes.py
+++ rickshaw/choose_archetypes.py
@@ -1,38 +1,38 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 13 22:59:19 2016
-
-@author: adam
-"""
-
-NICHE_ARCHETYPES = {
-    "mine" : {":agents:Source"},
-    "conversion" : {":agents:Source", ":agents:Sink"},
-    "enrichment" : {":agents:Source", ":agents:Sink", ":agents:Enrichment"},
-    "uo2_fabrication" : {":agents:Source", ":agents:Sink", ":agents:FuelFab"},
-    "triso_fabrication" : {":agents:Source", ":agents:Sink", ":agents:FuelFab"},
-    "fr" : {":agents:Source", ":agents:Sink", ":agents:Reactor"},
-    "lwr" : {":agents:Source", ":agents:Sink", ":agents:Reactor"},
-    "hwr" : {":agents:Source", ":agents:Sink", ":agents:Reactor"},
-    "htgr" : {":agents:Source", ":agents:Sink", ":agents:Reactor"},
-    "rbmk" : {":agents:Source", ":agents:Sink", ":agents:Reactor"},
-    "pb" : {":agents:Source", ":agents:Sink", ":agents:Reactor"},
-    "storage" : {":agents:Source", ":agents:Sink"},
-    "dry_reprocessing" : {":agents:Source", ":agents:Sink", ":agents:Separation"},
-    "purex" : {":agents:Source", ":agents:Sink", ":agents:Separation"},
-    "mox_fabrication" : {":agents:Source", ":agents:Sink", ":agents:FuelFab"},
-    "deep_geological_repository" : {":agents:Sink"},       
-    "near_surface_repository" : {":agents:Sink"}
-    }
-
-from niches import niches
+"""Modudle to help generate an archtypes list."""
 import random
 
-def choose_archetypes():
-    arche = []
-    for niche in niches:
+DEFAULT_SOURCES = {':agents:Source', ':cycamore:Source'}
+DEFAULT_SINKS = {':agents:Sink', ':cycamore:Sink'}
+
+NICHE_ARCHETYPES = {
+    "mine": {},
+    #"conversion" : {},
+    "enrichment": {":agents:Enrichment"},
+    "fuelfab:uo2_fabrication": {":agents:FuelFab"},
+    "triso_fabrication": {":agents:FuelFab"},
+    "reactor": {":agents:Reactor"},
+    "reactor:fr": {":agents:Reactor"},
+    "reactor:lwr": {":agents:Reactor"},
+    "hwr": {":agents:Reactor"},
+    "htgr": {":agents:Reactor"},
+    "rbmk": {":agents:Reactor"},
+    "pb": {":agents:Reactor"},
+    "storage": {},
+    "dry_reprocessing": {":agents:Separation"},
+    "purex": {":agents:Separation"},
+    "mox_fabrication": {":agents:FuelFab"},
+    "deep_geological_repository": {":agents:Sink"},
+    "near_surface_repository": {":agents:Sink"},
+    }
+
+
+def choose_archetypes(niches):
+    arches = [random.choose(NICHE_ARCHETYPES[niches[0]] | DEFAULT_SOURCES)]
+    for niche in niches[1:-1]:
         a = random.choose(NICHE_ARCHETYPES[niche])
-        arche.append(a)
-    return arche
-    
-    
+        arches.append(a)
+    if len(niches) > 1:
+        a = random.choose(NICHE_ARCHETYPES[niches][-1] | DEFAULT_SINK)
+        arches.append(a)
+    return arches
---
+++ rickshaw/generate.py
@@ -0,0 +1,30 @@
+"""Generates a random Cyclus input file."""
+
+import rickshaw.niches
+import rickshaw.choose_control
+import rickshaw.choose_archetypes
+
+
+def generate(max_num_niches=10):
+    """Creates a random Cyclus simulation input file dict.
+
+    Parameters
+    ----------
+    max_num_niches : int, optional
+        The maximum number of niches in the simulation
+
+    Returns
+    -------
+    inp : dict
+        A simulation dictionary in JSON form, suitable for use
+        as a Cyclus input file.
+    """
+    # intial structure
+    inp = {"simulation": {}}
+    sim = inp["simulation"]
+    sim["control"] = rickshaw.choose_control.choose_control()
+    # choose niches and archtypes
+    niches = rickshaw.niches.random_niches(max_niches=max_num_niches)
+    arches = rickshaw.choose_archtypes.choose_archetypes(niches)
+    return inp
+
