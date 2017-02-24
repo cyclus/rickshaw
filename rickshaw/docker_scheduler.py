03f4f29243e47e1cb1b8650fa8cd252dc7adc099
Anthony Scopatz <scopatz@gmail.com>
2017-02-24 16:52:59 -0500

    x

--- rickshaw/docker_scheduler.py
+++ rickshaw/docker_scheduler.py
@@ -37,7 +37,7 @@ class DockerScheduler(Scheduler):
                                                         publish_all_ports=True,
                                                                 detach=True)
         #cc = self.client.containers.get("cyclus_server")
-        while not cc.attrs['NetworkSetting']['IPAddress']:
+        while not cc.attrs['NetworkSettings']['Bridge']['IPAddress']:
             print("waiting on IP.")
             time.sleep(1)
         print("cyclus server started")
