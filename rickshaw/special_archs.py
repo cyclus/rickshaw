9e6bd2e2a8b128f744d70f233cda6a230c665b29
Flan <flanflanagan@gmail.com>
2017-03-24 16:14:42 -0500

    added some special functions

---
+++ rickshaw/special_archs.py
@@ -0,0 +1,48 @@
+import random 
+
+def generate_throwsink(commod, name):
+    vals = {}
+    vals["capacity"] = 1e299
+    vals["in_commods"] = commod 
+    config = {"name": name, "config": {"agents_sink": vals}}
+    return config
+
+def generate_throwsource(commod, name):
+    vals = {}
+    vals["capacity"] = 1e200
+    vals["commod"] = commod
+    vals["recipe_name"] = 'natural_uranium' 
+    config = {"name": name, "config": {"agents_source": vals}}
+    return config
+
+def enrich_tails(name, vals, commod):
+    vals[name] = 'tailcommod'
+    sink = generate_throwsink('tailcommod', 'enrichsink')
+    return sink
+
+def sep_streams(name, vals, commod):
+    sep_rand = random.uniform(0.0, 1.0)
+    sep = 1 - (0.0000001)**(1-sep_rand)*(0.1)**sep_rand
+    nucs = ["U", "Pu", "UPu", "Act"]
+    streams = {"streams":{
+                  "item":[{
+                      "commod": commod,
+                      "info": {
+                         "buf_size": 1e298,
+                         "efficiencies": {
+                             "item": []
+                         }
+                      }
+                  }]       
+              }}
+    choice = random.choice(nucs)
+    if choice == "U" or choice =="Pu":
+        temp = {"comp": choice, "eff": sep}
+        streams["streams"]["item"][0]["info"]["efficiencies"]["item"].append(temp)
+    vals[name] = streams["streams"]
+    return 0
+
+def ff_fill(name, vals, commod):
+    vals[name] = 'fillcommod'
+    source = generate_throwsource('fillcommod', 'ffsource')
+    return source
