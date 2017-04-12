d60019289ad953402e1a82d19c436934821980dc
Flan <flanflanagan@gmail.com>
2017-04-12 14:49:10 -0500

    update

--- rickshaw/docker_scheduler.py
+++ rickshaw/docker_scheduler.py
@@ -18,8 +18,9 @@ class DockerScheduler(Scheduler):
         try:
             try_test = self.client.nodes.list()
         except:
-            print('Remember to give this container/service access to the '+
-                   'docker socket on the host machine with the following command: '+
+            print( 'This container probably failed to connect to docker. \n'+
+                   'Remember to give this container/service access to the \n'+
+                   'docker socket on the host machine with the following command: \n'+
                    '-v /var/run/docker.sock:/var/run/docker.sock')       
         self.cyclus_container = None
         self.server_tag = "ergs/cyclus-server-dev"
