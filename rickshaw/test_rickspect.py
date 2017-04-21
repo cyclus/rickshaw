626f3e9033988809c4d3e9669d62678a3111ae2b
Flan <flanflanagan@gmail.com>
2017-04-21 09:59:36 -0500

    added test spect

---
+++ rickshaw/test_rickspect.py
@@ -0,0 +1,16 @@
+{
+    'simulation': {},
+    'niches': {"mine": {"enrichment"}, 
+                "enrichment": {"fuel_fab:uo2", "reactor:hwr"}, 
+                "fuel_fab:uo2": {"reactor:lwr"}, 
+                "reactor:hwr": {"repository"} ,
+                "reactor:lwr":{"repository"}
+              },
+    'archetypes': {"mine": {":cycamore:Source"},
+                  "enrichment": {":cycamore:Enrichment"},
+                  "fuel_fab:uo2": {":cycamore:FuelFab"},
+                  "reactor:hwr": {":cycamore:Reactor"},
+                  "reactor:lwr": {":cycamore:Reactor"},
+                  "repository": {":cycamore:Sink"}
+                 }
+}
