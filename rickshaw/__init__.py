9157ac6d679345bfdbb46d39554e10b35f4fee86
Anthony Scopatz <scopatz@gmail.com>
2017-05-24 16:36:55 -0400

    log more

--- rickshaw/__init__.py
+++ rickshaw/__init__.py
@@ -2,9 +2,10 @@ import logging
 from pythonjsonlogger import jsonlogger
 
 logger = logging.getLogger()
-
 logHandler = logging.StreamHandler()
-formatter = jsonlogger.JsonFormatter()
+formatter = jsonlogger.JsonFormatter(fmt='%(message)s %(asctime)s %(created)f '
+                                         '%(pathname)s %(funcName)s %(levelname)s '
+                                         '%(lineno)d %(process)d %(processName)s')
 logHandler.setFormatter(formatter)
 logger.addHandler(logHandler)
 logger.setLevel('INFO')
