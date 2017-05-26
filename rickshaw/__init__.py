a1d77c0fe74f87704841114e42e7815894024a7f
Flan <flanflanagan@gmail.com>
2017-05-26 11:45:36 -0500

    blash

--- rickshaw/__init__.py
+++ rickshaw/__init__.py
@@ -2,12 +2,13 @@ import logging
 from pythonjsonlogger import jsonlogger
 
 logger = logging.getLogger()
+logging.basicConfig(filename='myapp.log', level=logging.INFO)
 logHandler = logging.StreamHandler()
 formatter = jsonlogger.JsonFormatter(fmt='%(message)s %(asctime)s %(created)f '
                                          '%(pathname)s %(funcName)s %(levelname)s '
                                          '%(lineno)d %(process)d %(processName)s')
 logHandler.setFormatter(formatter)
 logger.addHandler(logHandler)
-logging.basicConfig(filename='myapp.log', level=logging.INFO)
+
 
 __version__ = "0.0.1"
